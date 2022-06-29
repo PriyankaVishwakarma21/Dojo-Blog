cd818a84c9eab698e8881e489ff915f3f1f55c23
Priyanka <vishwakarmapriyanka84@gmail.com>
2022-06-29 14:26:01 +0530

    third commit

--- src/BlogList.js
+++ src/BlogList.js
@@ -6,6 +6,7 @@ const BlogList = ({ blogs }) => {
         {blogs.map(blog => (
           <div className="blog-preview" key={blog.id} >
             <Link to={`/blogs/${blog.id}`} >
+              <h3>{blog.id}</h3>
             <h2>{ blog.title }</h2>
             <p>Written by { blog.author }</p>
             </Link>
